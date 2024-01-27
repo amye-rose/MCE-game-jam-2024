9652b46ae2f5a7d79e4b6cba01e47c2e7d994506
Adrien Brake <46970803+TigerInBoots@users.noreply.github.com>
2024-01-27 11:47:54 -0330

    Add files via upload

--- game.py
+++ game.py
@@ -1,54 +1,69 @@
 import pygame
-from pygame.locals import *
+pygame.init()
 
-from characters import *
-from controls import *
+# Creates the window the game will be played in
+win = pygame.display.set_mode((500,500))
 
-class App:
-    def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self.size = self.weight, self.height = 640, 480
-        self._player = Character("Images/Depression.jpg",320,240,(100,100))
-        self._controller = Control(self._player)
+# This loads the image that's used for the character sprite
+char = pygame.image.load('img/fire.png')
 
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._display_caption = pygame.display.set_caption("[GAME NAME HERE]")
-        self._running = True
-    
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
- 
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
-    
-    def on_event(self, event):
+# Class that hold all the variables for player's coordinates and character other character information
+class Player(object):
+    def __init__(self,x,y,width,height):
+        # I'm leaving out information regarding whether the character is facing left or right, because I'm not sure
+        # If we're doing walk cycle animations or not
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.velocity = 5
+        self.isJump = False
+        self.jumpCount = 10
+
+    def draw(win):
+        win.blit(char,(player.x,player.y))
+
+def redrawGameWindow():
+    win.fill((0,0,0))
+    win.blit(char,(player.x,player.y))
+    pygame.display.update()
+
+# This calls the "Player" class and basically just creates an instance of our character 
+# So we can use the variables stored in the class
+player = Player(300,410,64,64)
+run = True
+# While "run" is True, this loop will be active and the game will run
+while run:
+    pygame.time.delay(50)
+    # Checks if the game has been quit out of, and if it has, sets "run" to False and ends the loop
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            self._running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == K_ESCAPE:
-                self._running = False
-                return
-            if event.key == K_SPACE:
-                self._controller.jump()
-                
-    def on_loop(self):
-        keys = pygame.key.get_pressed()
-        self._controller.horizontalMovement(keys)
+            run = False
 
-    def on_render(self):
-        self._player.render(self._display_surf)
-        pygame.display.update()
+    keys = pygame.key.get_pressed()
 
-    def on_cleanup(self):
-        pygame.quit()
+    # These control horizontal movement
+    if keys[pygame.K_LEFT]:
+        player.x -= player.velocity
+    if keys[pygame.K_RIGHT]:
+        player.x += player.velocity
 
-game1 = App()
-game1.on_execute()
+    # Checks is "isJump" is False, and if the spacebar is pressed, sets that value to True
+    if not (player.isJump):
+        if keys[pygame.K_SPACE]:
+            player.isJump = True
+    else:
+        # I'll explain how this works later, but basically this is what happens when "isJump" is True
+        if player.jumpCount >= -10:
+            neg = 1
+            if player.jumpCount < 0:
+                neg = -1
+            player.y -= (player.jumpCount ** 2) * 0.5 * neg
+            player.jumpCount -= 1
+        else:
+            player.isJump = False
+            player.jumpCount = 10
+
+    redrawGameWindow()
+
+pygame.quit()
