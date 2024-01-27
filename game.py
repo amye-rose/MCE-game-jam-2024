755c933f75c90d557f0f0cd931fdfd526a5a7adc
Adrien Brake <adnbrake@gmail.com>
2024-01-27 11:40:52 -0330

    Added movement

--- game.py
+++ game.py
@@ -2,36 +2,21 @@ import pygame
 from pygame.locals import *
 
 from characters import *
+from controls import *
 
 class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
         self.size = self.weight, self.height = 640, 480
-        self._player = Character("Images\Depression.jpg",320,240,(100,100))
+        self._player = Character("Images/Depression.jpg",320,240,(100,100))
+        self._controller = Control(self._player)
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._display_caption = pygame.display.set_caption("[GAME NAME HERE]")
         self._running = True
-    
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self._running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == K_ESCAPE:
-                self._running = False
-    
-    def on_loop(self):
-        pass
-
-    def on_render(self):
-        self._player.render(self._display_surf)
-        pygame.display.update()
-
-    def on_cleanup(self):
-        pygame.quit()
     
     def on_execute(self):
         if self.on_init() == False:
@@ -43,6 +28,27 @@ class App:
             self.on_loop()
             self.on_render()
         self.on_cleanup()
+    
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_ESCAPE:
+                self._running = False
+                return
+            if event.key == K_SPACE:
+                self._controller.jump()
+                
+    def on_loop(self):
+        keys = pygame.key.get_pressed()
+        self._controller.horizontalMovement(keys)
+
+    def on_render(self):
+        self._player.render(self._display_surf)
+        pygame.display.update()
+
+    def on_cleanup(self):
+        pygame.quit()
 
 game1 = App()
 game1.on_execute()
